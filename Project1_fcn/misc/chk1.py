7039c628e447a163cea76402664385c5d5d26ad5
Deepanshu Lulla <deepanshu.lulla@gmail.com>
2016-02-02 03:27:37 -0500

    removed side files

--- Project1_fcn/misc/chk1.py
+++
@@ -1,6 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('www.google.com', 443))
-sslSocket = socket.ssl(s)
-print sslSocket
-s.close()
--- Project1_fcn/misc/comm_line.py
+++
@@ -1,5 +0,0 @@
-from __future__ import print_function
-import socket
-import sys
-
-print(sys.argv[1])
